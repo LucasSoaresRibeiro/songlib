b440bab0bc65351c58577cb09db8f05e213e299b
Lucas Ribeiro <ribeiro.lucas.soares@gmail.com>
2025-04-16 12:44:53 -0300

    feat: Auto backup

--- cmd_run_songlib_backup.ps1
+++ cmd_run_songlib_backup.ps1
@@ -1,7 +1,7 @@
 # conda deactivate
 # conda activate base
-& "C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" "$PSScriptRoot\songlib_backup.py"
-& "C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" "$PSScriptRoot\songlib_relation.py"
+& "C:\Lucas\Programas\ArcGISPro\envs\arcgispro-clone-pro-32\python.exe" "$PSScriptRoot\songlib_backup.py"
+& "C:\Lucas\Programas\ArcGISPro\envs\arcgispro-clone-pro-32\python.exe" "$PSScriptRoot\songlib_relation.py"
 git add *
 git commit -m "feat: Auto backup"
 git push
