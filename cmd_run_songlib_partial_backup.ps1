61124f782ad86a8800f3abad0ad167e60b48c0ae
Lucas Ribeiro <ribeiro.lucas.soares@gmail.com>
2025-05-07 09:59:19 -0300

    feat: Script updated

--- cmd_run_songlib_partial_backup.ps1
+++ cmd_run_songlib_partial_backup.ps1
@@ -10,8 +10,8 @@ param (
 # Execute the partial backup with credentials as arguments
 & "C:\Lucas\Programas\ArcGISPro\envs\arcgispro-clone-pro-32\python.exe" "$PSScriptRoot\songlib_partial_backup.py" --username $username --password $password
 
-# # Commit and push changes
-# git add *
-# git commit -m "feat: Auto partial backup"
-# git push
-# TIMEOUT 15
+# Commit and push changes
+git add *
+git commit -m "feat: Auto partial backup"
+git push
+TIMEOUT 15
